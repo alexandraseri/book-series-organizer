dbac7a24be4c143b21e7f4b526c24e20276a4733
Alexandra <salexa92@gmail.com>
2017-07-13 20:50:51 +0300

    update server file with static folders and mongoose connection

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require("express");
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 
+mongoose.connect('mongodb://localhost:27017');
 var server = express();
+
+server.set('view engine', 'jade');
+server.use(express.static(__dirname + '/public'));
+server.use('/scripts', express.static(__dirname + '/bower_components'));
+
 
 server.use('/', index);
 
