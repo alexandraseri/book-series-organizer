7880c94990c20b2b360d8273b43c780f79f0f83b
Alexandra <salexa92@gmail.com>
2017-07-14 22:18:30 +0300

    Added save new series client side

--- public/javascript/books-app.js
+++ public/javascript/books-app.js
@@ -1,4 +1,23 @@
 var app = angular.module('books-app', []);
-app.controller('books-controller', function($scope){
+app.controller('books-controller', function($scope, $http, $location, $window){
+    $scope.seriesName = '';
 
+    $scope.addSeries = function(){
+        var url = $location.protocol() + '://' + $location.host() + ':' + $location.port() + '/addSeries';
+        $window.location.href = url;
+    };
+
+    $scope.createSeries = function(){
+        console.log($scope.seriesName);
+        $http.post('/series/save', {name: $scope.seriesName}, {headers: {'Content-Type': 'application/json'}}).then(function(response){
+            if(response.data.errors){
+                console.log('Errors!');
+            } else {
+                var url = $location.protocol() + '://' + $location.host() + ':' + $location.port();
+                $window.location.href = url;
+            }
+        }).catch(function(){
+            console.log('Server Error!');
+        });
+    }
 });
